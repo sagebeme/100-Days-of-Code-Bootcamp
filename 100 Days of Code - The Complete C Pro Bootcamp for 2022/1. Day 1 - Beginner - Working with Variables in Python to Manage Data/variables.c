e2a3006ec3aea36df42e20b45ad23d1d0316bebe
sagebeme <eudicas5k@gmail.com>
2022-10-30 01:44:04 +0300

    adding new line

--- 100 Days of Code - The Complete C Pro Bootcamp for 2022/1. Day 1 - Beginner - Working with Variables in Python to Manage Data/variables.c
+++ 100 Days of Code - The Complete C Pro Bootcamp for 2022/1. Day 1 - Beginner - Working with Variables in Python to Manage Data/variables.c
@@ -5,12 +5,12 @@ int main()
 	char city_name[200];
 	printf("What is the name of the city you grew up in? \n");
 	scanf("%s", &city_name);
-	printf("City name is: %s",city_name);
+	printf("City name is: %s \n",city_name);
 
 	char pet_name[200];
 	printf("what is the name of your pet? \n");
 	scanf("%s",&pet_name);
-	printf("pet name is: %s", pet_name);
+	printf("pet name is: %s \n", pet_name);
 
-	printf("the name of your band could be: %s %s", city_name,pet_name);
+	printf("the name of your band could be: %s %s \n", city_name,pet_name);
 }
