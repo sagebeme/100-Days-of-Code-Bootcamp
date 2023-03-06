ab365cdb7f07a29a16564a70d985306ebbc1d297
sagebeme <eudicas5k@gmail.com>
2023-03-06 20:22:19 +0300

    adding love score to the code

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2022/4. Day 4 - Beginner - Randomisation and Python Lists/day-4-1-start.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/4. Day 4 - Beginner - Randomisation and Python Lists/day-4-1-start.py
@@ -11,3 +11,7 @@ print(random_float)
 #prints numbers from 0.999999999 - 4.999999999
 random_float = random.random() * 5
 print(random_float)
+
+#love Calculator
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
