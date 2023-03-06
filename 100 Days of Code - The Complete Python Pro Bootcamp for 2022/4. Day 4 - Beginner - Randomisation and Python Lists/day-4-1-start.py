495c6b448a4eacad084638e9ed3e1ddd01b84a51
sagebeme <eudicas5k@gmail.com>
2023-03-06 20:19:01 +0300

    printing a random float upto 4.999

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2022/4. Day 4 - Beginner - Randomisation and Python Lists/day-4-1-start.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/4. Day 4 - Beginner - Randomisation and Python Lists/day-4-1-start.py
@@ -1,7 +1,13 @@
 import random
 
+#1 - 10
 random_integer = random.randint(1 , 10)
 print(random_integer)
 
+#0.000000000000 - 0.999999999999
 random_float = random.random()
+print(random_float)
+
+#prints numbers from 0.999999999 - 4.999999999
+random_float = random.random() * 5
 print(random_float)
