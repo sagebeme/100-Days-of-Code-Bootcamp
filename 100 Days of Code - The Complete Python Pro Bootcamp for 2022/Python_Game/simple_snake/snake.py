9da9d1956c09a1ce3a3b2fe385b677de4e834d49
sagebeme <eudicas5k@gmail.com>
2023-07-19 16:58:03 +0300

    setting up game window

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Python_Game/simple_snake/snake.py
@@ -0,0 +1,10 @@
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+width = 640
+height = 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Snake Game")
