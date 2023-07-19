7faad779b88c02605427e2d7c1b286fc8fc7f185
sagebeme <eudicas5k@gmail.com>
2023-07-19 17:01:04 +0300

    defining the game loop

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Python_Game/simple_snake/snake.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Python_Game/simple_snake/snake.py
@@ -18,3 +18,30 @@ snake_color = (0, 255, 0)
 food_pos = (100, 100)
 food_size = 20
 food_color = (255, 0, 0)
+
+# Set up the game loop
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update the game objects
+    # TODO: Implement snake movement and collision detection
+
+    # Draw the game objects
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, snake_color, (snake_pos[0][0], snake_pos[0][1], snake_size, snake_size))
+    pygame.draw.rect(screen, food_color, (food_pos[0], food_pos[1], food_size, food_size))
+
+    # Update the display
+    pygame.display.flip()
+
+    # Limit the frame rate
+    clock.tick(10)
+
+# Clean up the game resources
+pygame.quit()
