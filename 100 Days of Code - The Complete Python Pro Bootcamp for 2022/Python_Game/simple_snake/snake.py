8fd2f5672538ee94077d5f628c8151f73346f676
sagebeme <eudicas5k@gmail.com>
2023-07-19 16:59:51 +0300

    setting up game objects

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Python_Game/simple_snake/snake.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Python_Game/simple_snake/snake.py
@@ -8,3 +8,13 @@ width = 640
 height = 480
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake Game")
+
+# Set up the snake
+snake_pos = [(width // 2, height // 2)]
+snake_size = 20
+snake_color = (0, 255, 0)
+
+# Set up the food
+food_pos = (100, 100)
+food_size = 20
+food_color = (255, 0, 0)
